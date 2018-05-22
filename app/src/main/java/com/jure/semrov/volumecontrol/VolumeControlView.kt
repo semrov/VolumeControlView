3f453ae03ca54c85b1b1946001091308148874ef
Jure <jure.semrov@outlook.com>
2018-05-22 22:33:59 +0200

    Volume scale now support values from 5 to 20

--- app/src/main/java/com/jure/semrov/volumecontrol/VolumeControlView.kt
+++ app/src/main/java/com/jure/semrov/volumecontrol/VolumeControlView.kt
@@ -12,17 +12,20 @@ import android.view.View
 class VolumeControlView(context: Context, attrs: AttributeSet?) : View(context,attrs)
 {
     var volumeScale: Int = 10
-        set(scale) {if (scale < 5) field = 5 else if (scale > 100) field = 100 else field = scale}
+        set(scale) {if (scale < 5) field = 5 else if (scale > 20) field = 20 else field = scale}
     var currentVolume: Int = 50
         set(volume) { if (volume < 0) field = 0 else if (volume > 100) field = 100 else field = volume }
 
     var backgroundPaint = Paint()
     var levelPaint = Paint()
 
+    private val default_width = resources.getDimensionPixelSize(R.dimen.volume_control_default_width)
+    private val default_height = resources.getDimensionPixelSize(R.dimen.volume_control_default_height)
+
     init {
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.VolumeControlView)
 
-        //sets volume scale, valid values are anything between 5 and 100
+        //sets volume scale, valid values are anything between 5 and 20
         volumeScale = attributes.getInteger(R.styleable.VolumeControlView_volume_scale,10)
 
         // set current volume percentage to anything between 0 and 100
@@ -37,6 +40,8 @@ class VolumeControlView(context: Context, attrs: AttributeSet?) : View(context,a
         // TypedArray objects are a shared resource and must be recycled after use
         attributes.recycle()
     }
+
+
 
 
 }
