2171073a1063d4b909150043720827bef212505d
Jure <jure.semrov@outlook.com>
2018-05-23 01:03:05 +0200

    Volume control view shows correct number of lines

--- app/src/main/java/com/jure/semrov/volumecontrol/VolumeControlView.kt
+++ app/src/main/java/com/jure/semrov/volumecontrol/VolumeControlView.kt
@@ -1,6 +1,7 @@
 package com.jure.semrov.volumecontrol
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
@@ -18,6 +19,7 @@ class VolumeControlView(context: Context, attrs: AttributeSet?) : View(context,a
 
     var backgroundPaint = Paint()
     var levelPaint = Paint()
+    var textPaint = Paint()
 
     private val default_width = resources.getDimensionPixelSize(R.dimen.volume_control_default_width)
     private val default_height = resources.getDimensionPixelSize(R.dimen.volume_control_default_height)
@@ -37,11 +39,54 @@ class VolumeControlView(context: Context, attrs: AttributeSet?) : View(context,a
         // sets the background color of the lines
         backgroundPaint.color = attributes.getColor(R.styleable.VolumeControlView_background_color, Color.GRAY)
 
+        textPaint.color = Color.BLACK
+        textPaint.textSize = 30F;
+
         // TypedArray objects are a shared resource and must be recycled after use
         attributes.recycle()
     }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val widthMode = View.MeasureSpec.getMode(widthMeasureSpec)
+        val heightMode = View.MeasureSpec.getMode(heightMeasureSpec)
 
+        val widthSize = View.MeasureSpec.getSize(widthMeasureSpec)
+        val heightSize = View.MeasureSpec.getSize(heightMeasureSpec)
 
+        val width = when (widthMode)
+        {
+            View.MeasureSpec.EXACTLY -> widthSize
+            View.MeasureSpec.AT_MOST -> default_width
+            View.MeasureSpec.UNSPECIFIED -> default_width
+            else -> default_width
+        }
 
+        val height = when (heightMode)
+        {
+            View.MeasureSpec.EXACTLY -> heightSize
+            View.MeasureSpec.AT_MOST -> default_height
+            View.MeasureSpec.UNSPECIFIED -> default_height
+            else -> default_height
+        }
+
+        setMeasuredDimension(width,height)
+    }
+
+    private fun drawTile(left : Float, top : Float,right : Float,bottom : Float, paint : Paint, canvas: Canvas)
+    {
+        canvas.drawRect(left, top, right, bottom, paint)
+    }
+
+    override fun onDraw(canvas: Canvas)
+    {
+        val tileHeight = height / (volumeScale * 2)
+        for (i in 0..(volumeScale-1))
+        {
+            val top = (tileHeight*(i*2)).toFloat()
+            drawTile(0F,top, width.toFloat(), top + tileHeight,backgroundPaint,canvas)
+        }
+
+        val y = (tileHeight * ((volumeScale * 2))).toFloat()
+        canvas.drawText("Volume set at: " + currentVolume + "%", (width / 4).toFloat(),y,textPaint)
+    }
 }
